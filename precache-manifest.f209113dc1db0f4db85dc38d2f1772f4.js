ea76503a1b86422a6835bb559c7d4f1df2a83783
den <denysbraun85@gmail.com>
2020-07-05 12:39:41 +0300

    Updates

---
+++ precache-manifest.f209113dc1db0f4db85dc38d2f1772f4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6274aec10958ed3abc020282fc8f2114",
+    "url": "/lets-cook-it/index.html"
+  },
+  {
+    "revision": "8735e64a55a0b931e960",
+    "url": "/lets-cook-it/static/css/main.6ac00c35.chunk.css"
+  },
+  {
+    "revision": "a65fb649e2d066fe7274",
+    "url": "/lets-cook-it/static/js/2.4529b77d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lets-cook-it/static/js/2.4529b77d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8735e64a55a0b931e960",
+    "url": "/lets-cook-it/static/js/main.8a9d68dd.chunk.js"
+  },
+  {
+    "revision": "03f7e79b1b126f1a6d10",
+    "url": "/lets-cook-it/static/js/runtime-main.095c5e7e.js"
+  },
+  {
+    "revision": "5abdeed0d2d29a2c1dc2fed475fc9735",
+    "url": "/lets-cook-it/static/media/kitchen_bg.5abdeed0.jpg"
+  }
+]);
